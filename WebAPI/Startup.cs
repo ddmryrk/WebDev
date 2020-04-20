91e87c1531130570caf824e08bfc698e1f5f5aa8
denizd <deniz.demiryurek@paynet.com.tr>
2020-04-20 09:39:56 +0300

    2

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,6 +32,11 @@ namespace WebAPI
             services.AddSingleton<IBookService, BookManager>();
             services.AddSingleton<IBookDal, EfBookDal>();
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:4200"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +46,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
